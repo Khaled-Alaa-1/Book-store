0e184da728a3b9c4922a2ee44e3fcbed33d3e95b
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-07-21 21:23:21 +0200

    Update bookform.js

--- src/components/bookform.js
+++ src/components/bookform.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/booksSlice';
-
 function BookForm() {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
   const dispatch = useDispatch();
+
+  const categoryOptions = ['Fiction', 'Action', 'Nonfiction', 'Comedy'];
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,11 +15,14 @@ function BookForm() {
       item_id: `item${Date.now()}`,
       title,
       author,
+      category,
     };
     dispatch(addBook(newBook));
     setTitle('');
     setAuthor('');
+    setCategory('');
   };
+
   return (
     <div>
       <h2>Add New Book</h2>
@@ -44,10 +49,26 @@ function BookForm() {
             />
           </label>
         </div>
+        <div>
+          <label htmlFor="category-select">
+            Category:
+            <select
+              id="category-select"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="">Select a category</option>
+              {categoryOptions.map((category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
         <button type="submit">Add Book</button>
       </form>
     </div>
-  );
-}
-
-export default BookForm;
+      );
+    }
+    export default BookForm;
