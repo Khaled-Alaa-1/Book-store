e2982141ab47c2339ce98ae2779090141f9c1f4a
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-07-23 06:05:40 +0200

    Update bookform.js

--- src/components/bookform.js
+++ src/components/bookform.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/booksSlice';
-
 function BookForm() {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const [category, setCategory] = useState('');
   const dispatch = useDispatch();
-
-  const categoryOptions = ['Fiction', 'Action', 'Nonfiction', 'Comedy'];
-
+  const categoryOptions = [
+    { id: '1', value: 'Fiction' },
+    { id: '2', value: 'Action' },
+    { id: '3', value: 'Nonfiction' },
+    { id: '4', value: 'Comedy' },
+  ];
   const handleSubmit = (e) => {
     e.preventDefault();
     const newBook = {
-      item_id: `item${Date.now()}`,
       title,
       author,
       category,
@@ -25,13 +26,14 @@ function BookForm() {
   };
 
   return (
-    <div>
-      <h2>Add New Book</h2>
+    <div className="form-div">
+      <hr />
+      <h2>ADD NEW BOOK</h2>
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="title-input">
-            Title:
             <input
+              placeholder="Book title"
               type="text"
               id="title-input"
               value={title}
@@ -41,8 +43,8 @@ function BookForm() {
         </div>
         <div>
           <label htmlFor="author-input">
-            Author:
             <input
+              placeholder="Author"
               type="text"
               id="author-input"
               value={author}
@@ -52,22 +54,21 @@ function BookForm() {
         </div>
         <div>
           <label htmlFor="category-select">
-            Category:
             <select
               id="category-select"
               value={category}
               onChange={(e) => setCategory(e.target.value)}
             >
               <option value="">Select a category</option>
-              {categoryOptions.map((category) => (
-                <option key={category} value={category}>
-                  {category}
+              {categoryOptions.map((option) => (
+                <option key={option.id} value={option.value}>
+                  {option.value}
                 </option>
               ))}
             </select>
           </label>
         </div>
-        <button type="submit">Add Book</button>
+        <button type="submit">ADD BOOK</button>
       </form>
     </div>
   );
